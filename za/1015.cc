4b6c97c2d6b46cffcedd84b68a3bbc3cbdc37f53
Wenle <robinwen024@gmail.com>
2018-03-11 00:47:17 +0800

    za

---
+++ za/1015.cc
@@ -0,0 +1,40 @@
+#include<cstdio>
+#include<map>
+#include<set>
+#include<cstring>
+using namespace std;
+int m[20010];
+typedef map<int,int> mk;
+int main()
+{
+  int T;
+  scanf("%d",&T);
+  while(T--)
+  {
+    memset(m,0,sizeof(T));
+    set<int> ma;//405
+    map<int,int> mb;//2 405
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+      scanf("%d",&m[i]);
+    mb[1]=m[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+      for(mk::reverse_iterator j=mb.rbegin();j!=mb.rend();j++)
+      {
+        if(m[i]>(j->second))
+        {
+           int a=j->first+1;
+           if(mb.find(a)==mb.end()||mb[a]>m[i])
+           {
+              mb[a]=m[i];
+           }
+           break;
+        }
+      }
+    }
+    printf("%d\n",n-mb.rbegin()->first);
+   }
+   return 0;
+}
